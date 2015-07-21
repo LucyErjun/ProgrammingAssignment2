e0ef7ecc5fa2c6999cf1eecaa3a2ab70678cd629
LucyErjun <LucyErjun@users.noreply.github.com>
2015-07-21 16:56:35 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,24 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(...)) {
+rm <- NULL
+   cm <-NULL
+   set <- function(y) {
+   x <<- y
+   rm <<- NULL
+   cm <<-NULL
+   }
+  get <- function() x
+  setRowMean <- function(rmean) rm <<- rmean
+  getRowMean <- function() rm
+  setColMean <- function(cmean) cm <<- cmean
+  getColMean <- function() cm
+  list(set = set, get = get,
+           setRowMean = setRowMean,
+           getRowMean = getRowMean,
+           setColMean = setColMean,
+           getColMean = getColMean
 }
 
 
@@ -12,4 +28,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        rm<-x$getRowMean()
+  cm<-x$getColMean()
+  if(!is.null(rm)&!is.null(cm)) {
+    message("getting cached data of mean")
+    return(list(rowMean=rm,colMean=cm)) 
+  } 
+  data<-x$get()
+  rm<-rowMeans(data,...)
+  cm<-colMeans(data,...)
+  x$setRowMean(rm)
+  x$setColMean(cm)
+  list(rowMean=rm,colMean=cm)
 }
